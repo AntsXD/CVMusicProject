bac735c414ab4032753e377da6a9b6b0d154b207
Anthony Charbel <anthonycharbel28@gmail.com>
2026-01-22 20:14:54 +0200

    added video capture using opencv

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import face_recognition
 import face_recognition_models
-import cv2
+import numpy as np
+import cv2 as cv
 
-print(2)
 
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot Open")
+    exit()
+
+
+while True:
+    # ret is a boolean value that returns true if a frame is captured
+    ret, frame = cap.read();
+
+    if not ret:
+        print("Frame error")
+        break
+
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) == ord('q'): #this is in the documentation but idk what it means
+        #ah turns out it is just a way to exit by clicking q, cool
+        break
+
+
+#must kill capture before quitting program
+cap.release()
+cv.destroyAllWindows()
